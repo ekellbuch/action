1aa6bd9e990d725791b3d93d93885db453a81c25
Kelly Buchanan <ekellbuchanan@gmail.com>
2024-07-08 15:21:57 -0700

    forgot to ass data_transforms

--- src/action/data/data_transforms.py
+++ src/action/data/data_transforms.py
@@ -14,15 +14,15 @@ class Transform(object):
 class ZScore(Transform):
     """z-score channel activity."""
 
-    def __init__(self):
-        pass
+    def __init__(self, dim=0):
+        self.dim = dim
 
-    def __call__(self, sample):
+    def __call__(self, x):
         """
 
         Parameters
         ----------
-        sample : np.ndarray
+        x : np.ndarray
             input shape is (time, n_channels)
 
         Returns
@@ -31,21 +31,29 @@ class ZScore(Transform):
             output shape is (time, n_channels)
 
         """
-        sample -= np.mean(sample, axis=0)
-        std = np.std(sample, axis=0)
-        sample[:, std > 0] = (sample[:, std > 0] / std[std > 0])
+        mean = np.mean(x, axis=self.dim, keepdims=True)
+
+        # Subtract the mean from the array
+        sample = x - mean
+
+        # Calculate the standard deviation along the specified dimension
+        std = np.std(sample, axis=self.dim, keepdims=True)
+
+        # Normalize only where std is greater than 0
+        nonzero_std_mask = std > 0
+        sample = np.divide(sample, std, where=nonzero_std_mask, out=sample)
+
         return sample
 
     def __repr__(self):
         return 'ZScore()'
 
-class CollapseAlongDimension(Transform):
+class SumAlongDimension(Transform):
     def __init__(self, dim=-1):
         """
-        Initializes the normalization transform.
-        Sums along dimension dim=-1 and applies z-score normalization along dim=0
+        Sums along dimension dim
         Args:
-        - dim (int): The dimension along which to normalize. Default is -1 (last dimension).
+        - dim (int): The dimension along which to sum. Default is -1 (last dimension).
         """
         self.dim = dim
 
@@ -55,21 +63,77 @@ class CollapseAlongDimension(Transform):
 
         Args:
         - x (torch.Tensor): The input tensor.
-            input shape is (time, n_channels)
+            input shape is (time, n_days, n_channels)
 
         Returns:
         - torch.Tensor: The normalized tensor.
         """
         # Compute the sum along the specified dimension, keep dimensions for broadcasting
         sample = x.sum(self.dim)
-        # apply z-transformer along the time dimension
-        sample -= np.mean(sample, axis=0)
-        std = np.std(sample, axis=0)
-        sample[:, std > 0] = (sample[:, std > 0] / std[std > 0])
         return sample
 
     def __repr__(self):
-        return f'CollapseAlongDimension(%d)' % self.dim
+        return f'SumAlongDimension(%d)' % self.dim
+
+class AvgAlongDimension(Transform):
+    def __init__(self, dim=-1):
+      """
+      Average along dimension dim
+      Args:
+      - dim (int): The dimension along which to average
+      """
+      self.dim = dim
+
+    def __call__(self, x):
+      """
+      Averages the input tensor along dim
+
+      Args:
+      - x (torch.Tensor): The input tensor.
+          input shape is (time, n_days, n_channels)
+
+      Returns:
+      - torch.Tensor: The normalized tensor.
+      """
+      # Normalize along dimension:
+      x /= x.sum(self.dim, keepdims=True)
+      return x
+
+    def __repr__(self):
+        return f'AvgAlongDimension(%d)' % self.dim
+
+
+class MinMaxDimension(Transform):
+  def __init__(self):
+    """
+    Make data range 0-1.
+    """
+
+  def __call__(self, x):
+    """
+    Applies the normalization to the input tensor.
+
+    Args:
+    - x (torch.Tensor): The input tensor.
+        input shape is (time, n_days, n_channels)
+
+    Returns:
+    - torch.Tensor: The normalized tensor.
+    """
+    # Compute the sum along the specified dimension, keep dimensions for broadcasting
+    min_val = 0  # x.min()
+    max_val = x.max()
+    # Avoid division by zero
+    range_val = max_val - min_val
+    assert range_val > 0
+
+    # Apply min-max normalization
+    normalized_sample = (x - min_val) / range_val
+
+    return normalized_sample
+
+  def __repr__(self):
+    return f'MinMaxDimension(%d)' % self.dim
 
 
 if __name__ == '__main__':
