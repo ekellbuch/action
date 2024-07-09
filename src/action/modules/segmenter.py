42c01f6e17995bad6f7bf27dd821d534e6f99eb3
Kelly Buchanan <ekellbuchanan@gmail.com>
2024-07-09 14:49:42 -0700

    add config for comparison

--- src/action/modules/segmenter.py
+++ src/action/modules/segmenter.py
@@ -402,6 +402,7 @@ class SegmenterModule(BaseClassifierModule):
     return loss
 
   def on_train_epoch_end(self):
+    # TODO: check trainer epoch metric loader
     self._calc_agg_metrics(stage="epoch/train_")
     self._reset_agg_metrics()
 
